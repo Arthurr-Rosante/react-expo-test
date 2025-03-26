4082be247da63fb7a0e5251723937cddadd6c047
Arthurr-Rosante <arrosante@gmail.com>
2025-03-26 18:20:56 -0300

    Login Form

--- src/app/Index.tsx
+++ src/app/Index.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View } from "react-native";
+import Form from "../components/Form";
 import "./global.css";
 
 const Index = () => {
   return (
     <View>
-      <Text>Index</Text>
+      <Form />
     </View>
   );
 };
---
+++ src/schemas/loginSchema.ts
@@ -0,0 +1,13 @@
+import { z } from "zod";
+
+export const loginFormSchema = z.object({
+  username: z.string().optional(),
+  email: z.string().nonempty({ message: "Email é obrigatório." }),
+  password: z
+    .string()
+    .nonempty({ message: "Senha é obrigtória." })
+    .min(8, { message: "Mínimo oito caracteres" })
+    .max(20, { message: "Máximo vinte caracteres" }),
+});
+
+export type LoginFormSchema = z.infer<typeof loginFormSchema>;
