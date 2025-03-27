f514907f924981ec2c3d232abd608d482091ce10
vppontes <149399268+vppontes@users.noreply.github.com>
2025-03-26 23:05:31 -0300

    pagina de login

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,7 +1,7 @@
-import { View, Text, TextInput, Button, StatusBar } from "react-native";
+import { View, Text, TextInput, Button } from "react-native";
 import { loginFormSchema, LoginFormSchema } from "../schemas/loginSchema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
 import { SafeAreaView } from "react-native-safe-area-context";
 import React from "react";
 
@@ -9,34 +9,89 @@ import "../app/global.css";
 
 const Form = () => {
   const {
-    register,
+    control,
     handleSubmit,
     formState: { errors, isSubmitting },
     reset,
   } = useForm<LoginFormSchema>({
     resolver: zodResolver(loginFormSchema),
   });
-  const onSubmit: SubmitHandler<LoginFormSchema> = async () => {};
+
+  const onSubmit: SubmitHandler<LoginFormSchema> = async (data) => {
+    try {
+      await new Promise((resolve) => setTimeout(resolve, 250));
+      console.log(data);
+      reset();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <SafeAreaView>
       <View>
-        <TextField label="Username" />
-        <TextField label="Email" />
-        <TextField label="Password" />
-        <Button title="Login" />
+        <Controller control={control} name="username" render={({ field: { onChange, value } }) => (
+          <View>
+            <Text>Usuário: </Text>
+            <TextInput onChangeText={onChange} value={value || ""}/>
+          </View>
+        )} />
       </View>
+
+      <View>
+        <Controller control={control} name="email" render={({ field: { onChange, value } }) => (
+          <View>
+            <Text>Email: </Text>
+            <TextInput keyboardType="email-address" onChangeText={onChange} value={value}/>
+          </View>
+        )} />
+      </View>
+
+      <View>
+        <Controller control={control} name="password" render={({ field: { onChange, value } }) => (
+          <View>
+            <Text>Senha: </Text>
+            <TextInput secureTextEntry onChangeText={onChange} value={value}/>
+          </View>
+        )} />
+      </View>
+
+      <Button title={isSubmitting ? "Enviando..." : "Login"} onPress={handleSubmit(onSubmit)} disabled={isSubmitting} />
     </SafeAreaView>
   );
 };
 
-function TextField({ label }: { label: string }) {
-  return (
-    <View>
-      <Text>{label}:</Text>
-      <TextInput />
-    </View>
-  );
-}
+// --------- TESTE MAS NAO DEU NADA CERTO
+
+// interface ControllerTextInputProps {
+//   control: any;
+//   name: string;
+//   label: string;
+//   keyboardType: 'default' | 'email-addres';
+//   secureTextEntry: boolean;
+// }
+
+// function ControllerTextInput({control, name, label}: {control:any; name: string; label: string}) {
+//   <View>
+//     <Controller control={control} name={name} render={ ({ field: {onChange, value } }) => {
+//       <View>
+//       <Text>{ label }:</Text>
+//       <TextInput keyboardType={keyboardType} secureTextEntry={secureTextEntry} />
+//     </View>
+//     }} />
+//   </View>
+// }
+
+// function ControllerTextInput({ control, name, label, keyboardTypeProp='default', secureTextEntry=false}: ControllerTextInputProps) {
+//   return (
+//     <View>
+//       <Controller control={control} name={name} render={({ field: { onChange, value } }) => (
+//           <View>
+//             <Text>{label}:</Text>
+//             <TextInput onChangeText={onChange} value={value} keyboardType={keyboardType} secureTextEntry={secureTextEntry}
+//             />
+//     </View>
+//   )
+// }
 
 export default Form;
